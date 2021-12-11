f132e54ebcf2a88d242e902988b08cc62636f741
Ilya Kulyshkin <dikartone@gmail.com>
2021-12-11 21:19:28 +0300

    Initial Commit

---
+++ src/main/java/com/rfl/lifebuilder/LifeBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.rfl.lifebuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeBuilderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LifeBuilderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rfl/lifebuilder/LifeBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rfl.lifebuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeBuilderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
